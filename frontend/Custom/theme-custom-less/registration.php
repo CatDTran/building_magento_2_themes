37ab4cc6540999e06c3d4e4ed2c65102117226e8
carot <some-email@domain.com>
2018-04-06 22:01:15 -0500

    Theme registation

--- frontend/Custom/theme-custom-less/registration.php
+++ frontend/Custom/theme-custom-less/registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Magento/luma',
+    'frontend/Custom/less',
     __DIR__
 );
